ec341b8b24889ec786334bfddb76881318a6e167
almas hossain <almashossain7384@gmail.com>
2025-08-17 10:27:39 +0600

    dashboard done

--- src/app/(dashboard_page)/(settings)/privacy-policy/page.tsx
+++ src/app/(dashboard_page)/(settings)/privacy-policy/page.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
 
-const PrivacyPolicyPage = () => {
+"use client";
+import { Button } from "@/components/ui/button";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+
+const JoditEditor = dynamic(() => import("jodit-react"), {
+  ssr: false,
+});
+
+export default function PrivacyPolicyPage() {
+  const [content, setContent] = useState("");
+
+  console.log(content)
   return (
-    <div>
-      PrivacyPolicyPage
-    </div>
-  )
-}
+    <>
+      <JoditEditor
+        className="text-black "
+        value={content}
+        config={{
+          height: 600,
+          placeholder: "Write your terms and conditions here...",
+        }}
 
-export default PrivacyPolicyPage
+        onBlur={(newContent) => setContent(newContent)}
+      />
+
+
+      <Button
+        className="w-full mt-8 py-6 rounded-full cursor-pointer text-white font-semibold transition-all duration-200"
+        style={{
+          background:
+            "linear-gradient(90deg, #6523E7 0%, #023CE3 80%, #6523E7 100%)",
+        }}
+      >
+        Update
+      </Button>
+    </>
+  );
+}
--- src/app/(dashboard_page)/(settings)/terms-and-conditions/page.tsx
+++ src/app/(dashboard_page)/(settings)/terms-and-conditions/page.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import dynamic from "next/dynamic";
+import { useState } from "react";
 
-const TermsAndConditionsPage = () => {
+const JoditEditor = dynamic(() => import("jodit-react"), {
+  ssr: false,
+});
+
+export default function TermsAndConditionsPage() {
+  const [content, setContent] = useState("");
+
+  console.log(content)
   return (
-    <div>
-      TermsAndConditionsPage
-    </div>
-  )
-}
+    <>
+      <JoditEditor
+        className="text-black "
+        value={content}
+        config={{
+          height: 600,
+          placeholder: "Write your terms and conditions here...",
+        }}
 
-export default TermsAndConditionsPage
+        onBlur={(newContent) => setContent(newContent)}
+      />
+
+
+      <Button
+        className="w-full mt-8 py-6 rounded-full cursor-pointer text-white font-semibold transition-all duration-200"
+        style={{
+          background:
+            "linear-gradient(90deg, #6523E7 0%, #023CE3 80%, #6523E7 100%)",
+        }}
+      >
+        Update
+      </Button>
+    </>
+  );
+}
